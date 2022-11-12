fabf24fff3f7c2e4c5a5b39a46caa435919f594a
Moumita Ray <moumitacloud08@gmail.com>
2022-11-12 17:27:28 +0530

    updated

--- gamepanel/admin.py
+++ gamepanel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+class PanelAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Panel, PanelAdmin)
+admin.site.register(models.NumberMaster, PanelAdmin)
--- gamepanel/models.py
+++ gamepanel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Panel(models.Model):
+    userid = models.CharField(default='', max_length=10)
+    color = models.CharField(default='',max_length=10)
+    number = models.CharField(default='',max_length=2)
+    contractPoint = models.CharField(default='',max_length=10)
+    point = models.CharField(default='',max_length=10)
+    period = models.CharField(default='',max_length=10)
+    created = models.DateTimeField(auto_now_add = True)
+
+class NumberMaster(models.Model):
+    numberId = models.CharField(default='', max_length=10)
+    numberDesc = models.CharField(default='',max_length=10)
